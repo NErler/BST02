69bdbf622a93274c3a3ef24e09e989eeb40b569b
Eleni-Rosalina Andrinopoulou <e.andrinopoulou@erasmusmc.nl>
2019-12-24 21:52:50 +0100

    Update 06DataVisualization Demo

--- Demos/Basic use of R/06DataVisualization.R
+++ Demos/Basic use of R/06DataVisualization.R
@@ -11,8 +11,10 @@
 #' If you are using the package for the first time, you will have to first install it:
 # install.packages("xlsx") 
 # install.packages("lattice")
+# install.packages("ggplot2")
 library(survival)
 library(lattice)
+library(ggplot2)
 
 #' Basic plot with 1 variable
 plot(pbc$bili)
@@ -34,6 +36,13 @@ legend(30, 25, legend = c("male", "female"), col = c(1,2), pch = 16)
 hist(pbc$bili, breaks = 50)
 hist(pbc$bili, breaks = seq(min(pbc$bili), max(pbc$bili), length = 20))
 
+#' Multiple panels
+par(mfrow=c(2,2))
+hist(pbc$bili, prob=T)
+hist(pbc$chol, prob=T)
+hist(pbc$albumin, prob=T)
+hist(pbc$alk.phos, prob=T)
+
 #' Barchart
 barchart(pbc$sex)
 
@@ -43,6 +52,17 @@ boxplot(pbc$age ~ pbc$sex, ylab = "Age", xlab = "Gender")
 #' Mutivariate plot
 pairs(cbind(pbc$bili, pbc$chol, pbc$albumin))
 
+#' Density plots of bili per sex
+pbc_male_bili <- pbc$bili[pbc$sex == "m"]
+pbc_female_bili <- pbc$bili[pbc$sex == "f"]
+plot(density(pbc_male_bili), col = rgb(0,0,1,0.5), ylim = c(0,0.40),
+     main = "Density plots", xlab = "bili", ylab = "")
+polygon(density(pbc_male_bili), col = rgb(0,0,1,0.5), border = "blue")
+lines(density(pbc_female_bili), col = rgb(1,0,0,0.5))
+polygon(density(pbc_female_bili), col = rgb(1,0,0,0.5), border = "red")
+legend(20,0.03, c("male", "female"), 
+       col = c(rgb(0,0,1,0.5), rgb(1,0,0,0.5)), lty = 1)  
+
 #' Lattice family - smooth evolution of bili with age
 xyplot(bili ~ age, data = pbc, type = "smooth", lwd = 2)
 
@@ -51,7 +71,7 @@ xyplot(bili ~ age, group = sex, data = pbc, type = "smooth", lwd = 2, col = c("r
 
 #' Lattice family - smooth evolution with points of bili with age per sex
 xyplot(bili ~ age, group = sex, data = pbc, type = c("p", "smooth"), lwd = 2, col = c("red", "blue"))
-  
+
 #' Lattice family - smooth evolution with points of bili with age per sex (as separate panel)
 xyplot(bili ~ age | sex, data = pbc, type = c("p", "smooth"), lwd = 2, col = c("red"))   
 xyplot(bili ~ age | status, data = pbc, type = c("p", "smooth"), lwd = 2, col = c("red"))  
@@ -64,16 +84,41 @@ xyplot(bili ~ age | status, data = pbc, type = c("p", "smooth"), lwd = 2, col = 
 pbc$status <- factor(pbc$status, levels = c(0, 1, 2), labels = c("censored", "transplant", "dead"))
 xyplot(bili ~ age | status, data = pbc, type = c("p", "smooth"), lwd = 2, col = c("red"), layout = c(3,1))  
 
-#' Density plots of bili per sex
-pbc_male_bili <- pbc$bili[pbc$sex == "m"]
-pbc_female_bili <- pbc$bili[pbc$sex == "f"]
-plot(density(pbc_male_bili), col = rgb(0,0,1,0.5), 
-     main = "Density plots", xlab = "age", ylab = "")
-polygon(density(pbc_male_bili), col = rgb(0,0,1,0.5), border = "blue")
-lines(density(pbc_female_bili), col = rgb(1,0,0,0.5))
-polygon(density(pbc_female_bili), col = rgb(1,0,0,0.5), border = "red")
-legend(20,0.03, c("male", "female"), 
-       col = c(rgb(0,0,1,0.5), rgb(1,0,0,0.5)), lty = 1)  
+#' Lattice family - individual patient plot
+xyplot(bili ~ day, group = id, data = pbcseq, type ="l", col = "black")
 
+#' Lattice family - individual patient plot per status
+pbcseq$status <- factor(pbcseq$status, levels = c(0, 1, 2), labels = c("censored", "transplant", "dead"))
+xyplot(bili ~ day | status, group = id, data = pbcseq, type ="l", col = "black", layout = c(3,1),
+       grid = TRUE, xlab = "Days", ylab = "Serum bilirubin")
 
+#' ggplot family - plot age with bili
+#' Each sex has a different colour
+ggplot(pbc, aes(age, bili, colour = sex)) + 
+  geom_point()
 
+ggplot(pbc, aes(age, bili, colour = sex)) + 
+geom_point(alpha = 0.3) +
+geom_smooth()
+
+#' ggplot family - plot day with bili for patient 93
+#' A smoothed curve is added in blue
+ggplot(pbcseq[pbcseq$id == 93,], aes(day, bili)) +
+geom_line() +
+geom_smooth(colour='blue', span = 0.4) +
+labs(title = "Patient 93", subtitle = "Evolution over time", 
+     y = "Serum bilirubin", x = "Days")
+
+#' ggplot family - plot bili per stage
+ggplot(pbc, aes(stage, bili, group = stage)) +
+geom_boxplot() +
+labs(y = "Serum bilirubin", x = "Stage")
+
+#' ggplot family - density plot of bili per sex
+p <- ggplot(pbc, aes(bili, fill = sex)) +
+geom_density(alpha = 0.25) 
+p
+
+p + scale_fill_manual(values=c("#999999", "#E69F00"))
+
+                            
