8ddcea2d697c5c16fed9cedff0f1bab1c0329d4f
Nicole Erler <n.erler@erasmusmc.nl>
2020-01-06 14:15:32 +0100

    code added to compile and copy practicals files to webpage and to write the corresponding .md files

--- copyFiles.R
+++ copyFiles.R
@@ -18,7 +18,27 @@ write_Demos_md <- function(x) {
   )
 }
 
+write_Practicals_md <- function(x) {
+  files <- unique(gsub('.Rmd$|.html$', '', 
+                       dir(file.path(getwd(), 'Practicals', x))))
+  
+  filenames <- gsub("\\_", " ", files)
+  cat(
+    paste0("---\n",
+           "title: ", x, "\n",
+           "---\n\n"
+    ),
+    paste0(
+      "* ", filenames,
+      " [[html]](/practical/", x, "/", files, ".html)",
+      "\n"
+    ), file = paste0('website/content/practical/', x, '.md')
+  )
+}
 
+################################################################################
+## Demos
+################################################################################
 # Render all R files in Demos to html
 for (x in dir('Demos')) {
   Rfiles <- grep('.R$', dir(file.path('Demos', x)), value = TRUE)
@@ -37,6 +57,34 @@ for (x in dir('Demos')) {
 }
 
 
+################################################################################
+## Practicals
+################################################################################
+# Render all R files in Practicals to html
+for (x in list.dirs('Practicals', recursive = FALSE)) {
+  Rmd_files <- grep('.Rmd$', dir(x), value = TRUE)
+  sapply(file.path(x, Rmd_files), rmarkdown::render)
+}
+
+# Copy all .html files in folder Practicals to the corresponding folders in website/static/practical
+for (x in list.dirs('Practicals', recursive = FALSE)) {
+  x <- gsub('Practicals/', '', x)
+  dir.create(file.path('website/static/practical', x))
+  
+  html_files <- grep('.html$', dir(file.path(getwd(), 'Practicals', x),
+                                   full.names = TRUE), value = TRUE)
+  
+  file.copy(from = html_files,
+            to = file.path('website/static/practical', x),
+            overwrite = TRUE)
+  
+  write_Practicals_md(x)
+}
+
+
+################################################################################
+## update webpage
+################################################################################
 # remove docs folder
 unlink("docs", recursive = TRUE)
 
@@ -45,3 +93,4 @@ system2('hugo', args = "-s website")
 
 # does not work, but I don't know why...
 # system2('hugo server', args = "-s website")
+# Works when running in the Terminal though
