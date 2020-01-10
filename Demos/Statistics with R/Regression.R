5c07d59dee1a29431db3d1ea49f5e386ffd34b24
N.S. Erler <n.erler@erasmusmc.nl>
2020-01-10 18:23:51 +0100

    first setup demo for regression

---
+++ Demos/Statistics with R/Regression.R
@@ -0,0 +1,18 @@
+#' ---
+#' title: "Demo: Regression"
+#' subtitle: "NIHES BST02"
+#' author: "Nicole Erler, Department of Biostatistics, Erasmus Medical Center"
+#' date: "`r Sys.setenv(LANG = 'en_US.UTF-8'); format(Sys.Date(), '%d %B %Y')`"
+#' output: 
+#'   html_document:
+#'     toc: true
+#'     toc_float:
+#'       collapsed: false
+#' ---
+
+#+  echo = FALSE
+# This is not part of the demo. 
+# It just allows the output to be wider (to make the html look nicer)
+options(width = 105)
+
+#' ## Linear Regression
