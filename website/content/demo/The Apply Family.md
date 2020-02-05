c90c3089f78c5219cd553cf0bce7d5f2a72eab16
ERASMUSMC\015004 <e.andrinopoulou@erasmusmc.nl>
2020-02-05 16:31:09 +0100

    update webpage

---
+++ website/content/demo/The Apply Family.md
@@ -0,0 +1,6 @@
+---
+title: The Apply Family
+---
+
+ * 01TheApplyFamily [[R]](/demo/The_Apply_Family/01TheApplyFamily.R) [[html]](/demo/The_Apply_Family/01TheApplyFamily.html)
+ * 02ExtraProgrammingExamples [[R]](/demo/The_Apply_Family/02ExtraProgrammingExamples.R) [[html]](/demo/The_Apply_Family/02ExtraProgrammingExamples.html)
--- website/static/demo/Statistics_with_R/Regression.R
+++ website/static/demo/Statistics_with_R/Regression.R
@@ -19,10 +19,12 @@ options(width = 105)
 # ------------------------------------------------------------------------------
 
 #' ## Linear Regression
+#' ### Fitting the model
 #' Fit a linear regression model
 mod <- lm(Infant.Mortality ~ Fertility + Agriculture + Education + Catholic,
           data = swiss)
 
+#' ### Model results
 #' Get the model summary
 summary(mod)
 
@@ -38,6 +40,33 @@ confint(mod)
 confint(mod, level = 0.9, parm = -1)
 
 
+#' ### Model evaluation
+#' To evaluate how well the model fits the data we can check the diagnostic 
+#' plots that are obtained when plotting the model:
+#+ fig.width  = 8, fig.height = 6
+par(mfrow = c(2, 2), mar = c(4, 4.2, 2, 1.3)) # graphical settings
+plot(mod)
+
+#' We can re-create some of these plots ourselves.
+
+#' Residuals and fitted values can be obtained from a linear regression model in two ways:
+mod$residuals
+residuals(mod)
+
+mod$fitted.values
+fitted(mod) # also: fitted.values(mod)
+
+scatter.smooth(mod$fitted.values, mod$residuals, lpars = list(col = 'red'))
+abline(h = 0, lty = 2, col = 'grey')
+
+#' #### QQ-plot
+#' To create a normal QQ-plot we plot the quantiles of the standardized 
+#' residuals against the quantiles of a normal distribution:
+qqnorm(rstandard(mod))
+abline(a = 0, b = 1, lty = 1, col = 'grey')
+
+
+
 
 # ------------------------------------------------------------------------------
 #' ## Logistic Regression
@@ -46,3 +75,32 @@ summary(mod2)
 #' Note: the coefficients are on the log odds ratio scale.
 
 #' The functions `coef()` and `confint()` also work on GLMs.
+#' 
+#' 
+
+#' ### Changing parts of the model
+#' To re-fit the model with a small change, the function `update()` is useful:
+mod2b <- update(mod2, formula = . ~ . - education)
+
+#' In `update()` we specify the model we want to update and only those arguments
+#' that need to be changed. In our case, the model formula.
+#' Using `.` we can specify to keep parts of the formula the same.
+#' This allows us to "subtract" `education` from the original formula.
+
+#' `update()` also works for other types of objects (not just regression models):
+formula(mod2)
+update(formula(mod2), . ~ . - education)
+
+
+#' ### Comparing Models
+#' To check if `education` is needed in the model, we can use a likelihood ratio
+#' test to compare the models with and without `education`.
+#' This is implemented in the function `anova()`.
+anova(mod2, mod2b, test = "LRT")
+
+#' To compare non-nested models (where one model is not a special case of the
+#' other model) we can use the `AIC()` or `BIC()`:
+
+mod3 <- update(mod2b, formula = . ~ . - age + induced)
+
+AIC(mod3, mod2b)
